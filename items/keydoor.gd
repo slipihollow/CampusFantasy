c4aa20fa07b4c3636618ab8f20203f33a1851ba4
TheCaptainKiwi <56768066+TheCaptainKiwi@users.noreply.github.com>
2020-03-01 01:04:58 +0100

    Add files via upload

--- items/keydoor.gd
+++ items/keydoor.gd
@@ -4,6 +4,6 @@ func _ready():
 	$area.connect("body_entered", self, "body_entered")
 
 func body_entered(body):
-	if body.name == "player" and body.get("keys") > 0:
-		body.keys -= 1
+	if body.name == "player" and body.get("questies") == 9:
+		body.questies -= 9
 		queue_free()
